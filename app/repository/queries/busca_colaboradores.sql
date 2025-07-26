9fbf587718244c0d241e079d28c3464fa1d3c662
Pedro Leal <pedroleal360@gmail.com>
2025-07-25 22:14:29 -0300

    fix: correct SQL query structure and logic for fetching users with dependents

--- app/repository/queries/busca_colaboradores.sql
+++ app/repository/queries/busca_colaboradores.sql
@@ -1,19 +1,16 @@
-WITH users_cte as(
+WITH users_cte AS (
     SELECT
+        us.id,
         us.nome_completo,
-        CASE
-            WHEN deps.id is NULL THEN FALSE
-            ELSE TRUE
-        END AS have_dependents,
-        us.id_dp
+        us.id_dp,
+        COUNT(deps.id) > 0 AS have_dependents
     FROM users AS us
-    FULL JOIN dependentes AS deps ON us.id = deps.id_user
-    GROUP BY us.nome_completo
-    ORDER BY nome_completo ASC
+    LEFT JOIN dependentes AS deps ON us.id = deps.id_user
+    GROUP BY us.id, us.nome_completo, us.id_dp
 )
 SELECT
     us.nome_completo,
     us.have_dependents
 FROM users_cte AS us
 INNER JOIN departamentos AS deps ON us.id_dp = deps.id
-WHERE deps.id = :departamento_id
+WHERE deps.id = :departamento_id;
